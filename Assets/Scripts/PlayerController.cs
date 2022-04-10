6f28e61785030a6568b0a62bee0dc2ba4f457893
slivoviy <s01110011@yandex.ru>
2022-04-10 18:51:09 +0300

    added tilemap and animation for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,13 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour {
     private PlayerMovement playerMovementInput;
-    private Vector2 playerVelocity;
-    [SerializeField]
-    private float playerSpeed = 2.0f;
+    private Rigidbody2D playerBody;
+    [SerializeField] private float playerSpeed = 2.0f;
+    [SerializeField] private TilemapCollider2D wall;
+    [SerializeField] private Animator playerAnimation;
+    private static readonly int IsMoving = Animator.StringToHash("isMoving");
 
 
     private void Awake() {
@@ -22,8 +22,27 @@ public class PlayerController : MonoBehaviour {
         playerMovementInput.Disable();
     }
 
-    void Update() {
+    private void Start() {
+        playerBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate() {
         Vector2 movementInput = playerMovementInput.Player.Movement.ReadValue<Vector2>();
-        transform.Translate(movementInput * (Time.deltaTime * playerSpeed));
+
+        
+        playerBody.velocity = movementInput * playerSpeed;
+        if (movementInput != Vector2.zero) {
+            if (movementInput.x < 0) {
+                transform.eulerAngles = new Vector3(0, 180, 0);
+            }
+            else {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            }
+
+            playerAnimation.SetBool(IsMoving, true);
+        }
+        else {
+            playerAnimation.SetBool(IsMoving, false);
+        }
     }
 }
